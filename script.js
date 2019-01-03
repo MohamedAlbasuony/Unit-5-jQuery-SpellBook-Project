30edcefd724d1da0db9ff4a1a5dc86fdfb4d0e59
MohamedAlbasuony <43444147+MohamedAlbasuony@users.noreply.github.com>
2019-01-03 09:48:54 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,27 @@
+//Spell #1
+
+$("#chicken").click(function(){
+   $("#chicken").fadeOut();
+});
+
+$("#Roasted Chicken").click(function(){
+    $("#Roasted Chicken").fadeIn();
+});
+
+
+//Spell #2
+
+$("#dragon").click(function(){
+     $("#dragon").toggle();
+});
+
+
+
+//Spell #3
+
+$(" #goku").click(function(){
+     $("#goku").slideUp();
+});
+
+
+
